24ed46f0d90a1a655d782c718e69dabd8768c228
Orie Chinedu <oriechinedu@gmail.com>
2019-06-17 13:02:41 +0100

    Attach the actions to the buttons

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -11,10 +11,11 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
-  return { type: INCREMENT}
+  return { type: INCREMENT };
+
 };
 
 export const decrement = () => {
   // Fill in this function
-  return {type: DECREMENT}
+  return { type: DECREMENT };
 };
